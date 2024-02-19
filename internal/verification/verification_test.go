540c38d9be0de44312800dc5503cabe2df33c3be
EwvwGeN <doj54567467z@gmail.com>
2024-02-19 12:25:23 +0300

    test: add test for check that creating only one key per session

--- internal/verification/verification_test.go
+++ internal/verification/verification_test.go
@@ -32,3 +32,23 @@ func Test_Fall(t *testing.T) {
 	uuid = gofakeit.UUID()
 	assert.NotEqual(t, string(decodedUid), uuid)
 }
+
+func Test_OneKeyForSession(t *testing.T) {
+	uuid := gofakeit.UUID()
+
+	vCode, err := GenerateVerificationCode([]byte(uuid))
+	require.NoError(t, err)
+
+	decodedUid, err := DecryptVerificationCode(vCode)
+	require.NoError(t, err)
+
+	require.Equal(t, string(decodedUid), uuid)
+
+	_, err = GenerateVerificationCode([]byte(uuid))
+	require.NoError(t, err)
+
+	decodedUid, err = DecryptVerificationCode(vCode)
+	require.NoError(t, err)
+
+	require.Equal(t, string(decodedUid), uuid)
+}
